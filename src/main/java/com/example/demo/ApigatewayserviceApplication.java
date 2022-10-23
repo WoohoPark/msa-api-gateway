0b1966eef5e5d41179a2a5e0f67ae395229f02c7
Wooho <simaro0816@gamil.com>
2022-10-23 16:59:56 +0900

    Predicate,Filter,LB Add

--- src/main/java/com/example/demo/ApigatewayserviceApplication.java
+++ src/main/java/com/example/demo/ApigatewayserviceApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApigatewayserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayserviceApplication.class, args);
+		new SpringApplicationBuilder(ApigatewayserviceApplication.class)
+				.web(WebApplicationType.REACTIVE) // .REACTIVE, .SERVLET
+				.run(args);
+
+		//SPRING WEB FLUX SETTINGS
+
+//		SpringApplication.run(ApigatewayserviceApplication.class, args);
 	}
 
 }
